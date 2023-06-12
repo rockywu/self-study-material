f1a1e1ce954b4fc572d24a1ca3a56c602e9e0db3
rockywu 吴佳雷 <wjl19890427@hotmail.com>
2023-06-12 16:42:03 +0800

    添加函数重载片段

---
+++ snippets/createOverload.js
@@ -0,0 +1,33 @@
+/**
+ * 实现一个JS函数重载代码实现
+ * @example 
+ * const test = createOverload();
+ * test.addImpl('number', 'string', (num, str) => {
+ *  console.log(num, str)
+ * })
+ * test.addImpl('number', 'boolean', (num, bool) => {
+ *    console.log('2', num, bool)
+ * })
+ */
+function createOverload() {
+    const callMap = new Map();
+    function overload(...args) {
+        const key = args.map(arg => typeof arg).join(',')
+        const fn = callMap.get(key);
+        if(fn) {
+            fn.apply(this, args)
+        } else {
+            throw new RangeError('参数错误')
+        }
+    }
+    overload.addImpl = function(...args) {
+        const fn = args.pop();
+        if(typeof fn !== 'function') {
+            return;
+        }
+        callMap.set(args.join(','), fn)
+    }
+    return overload;
+}
+
+export default createOverload
